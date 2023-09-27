fd315aa04074f16de20872d627055cf7d42b01e0
malbong <gon3578@gmail.com>
2023-09-27 03:07:56 +0000

    ended 14 chapter

--- 07-class-based/src/components/User.js
+++ 07-class-based/src/components/User.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends React.Component {
+  componentWillUnmount() {
+    console.log('user unmount');
+  }
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
