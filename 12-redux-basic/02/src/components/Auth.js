4f27f4716309ac4d328b2ccc91a778bf9f9fbb60
malbong <gon3578@gmail.com>
2023-10-02 06:16:49 +0000

    ended 19 chapter

--- 12-redux-basic/02/src/components/Auth.js
+++ 12-redux-basic/02/src/components/Auth.js
@@ -1,17 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/auth';
+
 import classes from './Auth.module.css';
+import UserProfile from './UserProfile';
 
 const Auth = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isAuthenticated);
+
+  const dispatch = useDispatch();
+  const authHandler = (e) => {
+    e.preventDefault();
+
+    dispatch(authActions.login());
+  };
+
+  if (isLoggedIn) return <UserProfile />;
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={authHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
           </div>
           <button>Login</button>
         </form>
