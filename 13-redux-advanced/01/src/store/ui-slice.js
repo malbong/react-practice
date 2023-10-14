9cc07d40aa8bfe660b8c84b9f170219accf16547
malbong <gon3578@gmail.com>
2023-10-14 22:16:06 +0900

    redux with useEffect

--- 13-redux-advanced/01/src/store/ui-slice.js
+++ 13-redux-advanced/01/src/store/ui-slice.js
@@ -1,11 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
   name: 'cart',
-  initialState: { cartIsVisible: false },
+  initialState: {
+    cartIsVisible: false,
+    notification: null,
+  },
   reducers: {
-    toggle(state) {state.cartIsVisible = !state.cartIsVisible; }
-  }
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      if (action.payload === null) {
+        state.notification = null;
+      } else {
+        state.notification = {
+          status: action.payload.status,
+          title: action.payload.title,
+          message: action.payload.message,
+        };
+      }
+    },
+  },
 });
 
 export const uiActions = uiSlice.actions;
