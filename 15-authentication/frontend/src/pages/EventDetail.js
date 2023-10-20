9e0ba2494dfe31e1dbd607716b925437a82084e6
malbong <gon3578@gmail.com>
2023-10-20 08:32:49 +0000

    ended 22 chapter

--- 15-authentication/frontend/src/pages/EventDetail.js
+++ 15-authentication/frontend/src/pages/EventDetail.js
@@ -9,6 +9,7 @@ import {
 
 import EventItem from '../components/EventItem';
 import EventsList from '../components/EventsList';
+import { getAuthToken } from '../util/auth';
 
 function EventDetailPage() {
   const { event, events } = useRouteLoaderData('event-detail');
@@ -76,10 +77,16 @@ export async function loader({ request, params }) {
   });
 }
 
+// delete Event
 export async function action({ params, request }) {
   const eventId = params.eventId;
+  const token = getAuthToken();
+
   const response = await fetch('http://localhost:8080/events/' + eventId, {
     method: request.method,
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
   });
 
   if (!response.ok) {
